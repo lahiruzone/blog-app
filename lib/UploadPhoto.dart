5a026df391b0d2646f9d800e0f137278407a0869
Dinushan Rajapaksha <lahiruzone@gmail.com>
2019-09-19 11:49:28 +0530

    firebase storage

--- lib/UploadPhoto.dart
+++ lib/UploadPhoto.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-// import 'package:intl/intl.dart';
-// import 'package:firebase_database/firebase_database.dart';
-// import 'package:firebase_storage/firebase_storage.dart';
+import 'package:intl/intl.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'home.dart';
 
 class UploadPhoto extends StatefulWidget{
   State<StatefulWidget> createState(){
@@ -15,6 +16,7 @@ class _UploadPhotoState extends State<UploadPhoto>{
 
   File sampleImage;
   String _myValue;
+  String url;
   final formkey = new GlobalKey<FormState>();
 
   @override
@@ -53,13 +55,61 @@ class _UploadPhotoState extends State<UploadPhoto>{
     }
   }
 
+  void uploadStatus() async{
+    if(validateAndSave()){
+      final StorageReference postImageRef = FirebaseStorage.instance.ref().child("Post_Images");
+      
+      //random key for image name
+      var timeKey =  new DateTime.now();
+
+      final StorageUploadTask uploadTask = postImageRef.child(timeKey.toString()  + "jpg").putFile(sampleImage);
+      var imageUrl = await(await uploadTask.onComplete).ref.getDownloadURL();
+      url = imageUrl.toString();
+
+      goToHomePage;
+      saveToDatabase(url);
+
+    }
+  }
+
+  void saveToDatabase(url){
+    //get date and time separatly
+    var dbTimeKey =  new DateTime.now();
+    var formatDate = new DateFormat('MMM d, yyyy');
+    var formatTime = new DateFormat('EEEE, hh:mm aaa');
+
+    String date = formatDate.format(dbTimeKey);
+    String time = formatTime.format(dbTimeKey);
+
+    //save to database
+    DatabaseReference ref = FirebaseDatabase.instance.reference();
+
+    var data ={
+      // "image": url,
+      "description": _myValue,
+      "date": date,
+      "time": time,
+    };
+
+    ref.child("Posts").push().set(data);
+  }
+
+  void goToHomePage(){
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context){
+        return new Home();
+      })
+    );
+  }
+
   Widget enableUpload(){
     return new Container(
       child: new Form(
         key: formkey,
         child: new Column(
           children: <Widget>[
-            Image.file(sampleImage, height: 280.0, width: 560.0,),
+            Image.file(sampleImage, height: 200.0, width: 400.0,),
             SizedBox(height: 15.0,),
             TextFormField(
               decoration: new InputDecoration(labelText: 'Description'),
@@ -70,17 +120,23 @@ class _UploadPhotoState extends State<UploadPhoto>{
                 return _myValue = value;
               },
             ),
-            SizedBox(height: 15.0,),
-            RaisedButton(
-              elevation: 10.0,
-              child: Text("Add a new Post"),
-              textColor: Colors.white,
-              color: Colors.blue,
-              onPressed: validateAndSave,
-            ),
+            SizedBox(height: 80.0,),
+                MaterialButton( 
+                  height: 45.0,
+                  minWidth: 300.0, 
+                  color: Theme.of(context).primaryColor, 
+                  textColor: Colors.white,
+                  child: Text("Add a new Post"),
+                  splashColor: Colors.greenAccent,
+                  onPressed: uploadStatus,
+                  shape: new RoundedRectangleBorder(
+                     borderRadius: new BorderRadius.circular(20.0)),
+                     
+                ),
           ],
         ),
       )
     );
   }
 }
+
